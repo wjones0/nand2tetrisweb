2900ec1d2caacfea7224d06906f29e1d4bf5900c
William Jones <turam0@hotmail.com>
2015-04-13 21:39:42 -0400

    Initial commit

---
+++ Nand2TetrisWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Nand2TetrisWeb.Startup))]
+namespace Nand2TetrisWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

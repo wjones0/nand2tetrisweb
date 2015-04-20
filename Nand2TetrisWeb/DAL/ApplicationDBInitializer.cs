576abce38393a6dbd72e55bdf03dec080eedeb03
William Jones <turam0@hotmail.com>
2015-04-19 21:59:18 -0400

    seed data update

--- Nand2TetrisWeb/DAL/ApplicationDBInitializer.cs
+++ Nand2TetrisWeb/DAL/ApplicationDBInitializer.cs
@@ -1,4 +1,5 @@
 ﻿using Contracts;
+using Microsoft.AspNet.Identity;
 using Nand2TetrisWeb.Models;
 using System;
 using System.Collections.Generic;
@@ -8,13 +9,38 @@ using System.Web;
 
 namespace Nand2TetrisWeb.DAL
 {
-    public class ApplicationDBInitializer :  DropCreateDatabaseAlways<ApplicationDbContext> //CreateDatabaseIfNotExists<ApplicationDbContext>  //
+    public class ApplicationDBInitializer : DropCreateDatabaseAlways<ApplicationDbContext> //CreateDatabaseIfNotExists<ApplicationDbContext>  //
     {
         protected override void Seed(ApplicationDbContext context)
         {
-            var srcFile = new SourceFile() { FileName="test", FileBody="testBody", CreateDate=DateTime.Now, ModifyDate=DateTime.Now };
-            context.SourceFiles.Add(srcFile);
+
+            var passwordHash = new PasswordHasher();
+            string password = passwordHash.HashPassword("bobobo");
+            context.Users.Add(new ApplicationUser()
+                {
+                    UserName = "bob@bob.com",
+                    PasswordHash = password,
+                    Email = "bob@bob.com",
+                    SecurityStamp = Guid.NewGuid().ToString()
+                });
+
             context.SaveChanges();
+
+            var bob = (from u in context.Users
+                           where u.Email == "bob@bob.com"
+                           select u).FirstOrDefault();
+
+            context.SourceFiles.Add(new SourceFile()
+            {
+                CreateDate = DateTime.Now,
+                ModifyDate = DateTime.Now,
+                FileName = "Nand.hdl",
+                FileBody = "/* Nand file \n */",
+                userid = new Guid(bob.Id)
+            });
+            
+
+
         }
     }
 }

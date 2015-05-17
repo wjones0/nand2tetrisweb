aa636b1ba54b8f055d970107007eee6f59c4c3fe
William Jones <turam0@hotmail.com>
2015-05-17 17:25:17 -0400

    stub out UI for Assembler

---
+++ Nand2TetrisWeb/Controllers/AssemblerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Contracts;
+
+namespace Nand2TetrisWeb.Controllers
+{
+    public class AssemblerController : Controller
+    {
+        // GET: Assembler
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public JsonResult Assemble(string asmText)
+        {
+            return Json(new AssembledFile() { asmText = asmText, hackText = asmText });
+        }
+    }
+}
